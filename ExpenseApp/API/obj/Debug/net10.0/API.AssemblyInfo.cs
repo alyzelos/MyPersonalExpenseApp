f09e5c62bd3be182305d952e7d5f64cc42602327
Pojar Cosmin Alin <pojarcosminalin@gmail.com>
2026-08-13 18:48:29 +0300

    cleanning

--- ExpenseApp/API/obj/Debug/net10.0/API.AssemblyInfo.cs
+++ ExpenseApp/API/obj/Debug/net10.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+617692493d18ef761fb07d16242d25e8324d0ba3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d3eb22e51a6cbc94826984654bb785b898da18a5")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
