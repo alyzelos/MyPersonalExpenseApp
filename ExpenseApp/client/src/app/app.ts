40275f959ecc827ebdd56f4997db213c583492a4
Pojar Cosmin Alin <pojarcosminalin@gmail.com>
2026-08-17 15:29:04 +0300

    adding angular material to project

--- ExpenseApp/client/src/app/app.ts
+++ ExpenseApp/client/src/app/app.ts
@@ -1,12 +1,26 @@
-import { Component, signal } from '@angular/core';
+import { NgFor } from '@angular/common';
+import { HttpClient } from '@angular/common/http';
+import { Component, inject, OnInit, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NgFor],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
-export class App {
-  protected readonly title = signal('client');
+export class App implements OnInit {
+  public readonly title = signal('MyPersonalExpenseApp');
+  http = inject(HttpClient);
+  constructor(private httpClient: HttpClient){}
+  categories: any;
+
+  ngOnInit(): void {
+    this.http.get('https://localhost:5001/api/categories').subscribe({
+      next: response => this.categories= response,
+      error: error => console.log(error),
+      complete: () => console.log('request has completed')
+    })
+  }
+
 }
